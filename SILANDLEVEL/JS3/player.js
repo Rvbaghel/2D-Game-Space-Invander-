a3b9cf3c8479e03c377ac5bfe7ce327d8116a95f
Vishal_Baghel_28 <136146139+Rvbaghel@users.noreply.github.com>
2024-01-22 11:36:10 +0530

    Add files via upload
    Complete

---
+++ SILANDLEVEL/JS3/player.js
@@ -0,0 +1,81 @@
+export default class Player {
+    rightpressed = false;
+    leftpressed = false;
+    shootepressed =false;
+
+    constructor(canvas, velocity,bulletController) {
+        this.canvas = canvas;
+        this.velocity = velocity;
+        this.bulletController =bulletController;
+
+        this.x = this.canvas.width / 2;
+        this.y = this.canvas.height - 75;
+
+        this.width = 70;
+        this.height = 56;
+
+        this.image = new Image();
+
+        this.image.src = "picture/player.png";
+
+        document.addEventListener("keyup", this.keyup);
+        document.addEventListener("keydown", this.keydown);
+
+    }
+    draw(ctx) {
+        
+        if(this.shootepressed){
+            this.bulletController.shoot(this.x+this.width/2,this.y,4,10)
+           
+        }
+        this.move();
+
+        this.conbountreys();
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+
+    conbountreys(){
+        if(this.x<0){
+            this.x =0;
+        }
+        if(this.x >this.canvas.width - this.width)
+        {
+            this.x =this.canvas.width -this.width;
+        }
+    }
+    move(){
+        if (this.rightpressed) {
+            this.x += this.velocity;
+        }
+        else if (this.leftpressed) {
+            this.x += -this.velocity;
+        }
+    }
+
+    keydown = (event) => {
+        if (event.code == "ArrowRight") {
+            this.rightpressed = true;
+
+        }
+        if (event.code == "ArrowLeft") {
+            this.leftpressed = true;
+        }
+        if(event.code == "Space"){
+            this.shootepressed =true;
+
+        }
+    };
+
+    keyup = (event) => {
+        if (event.code == "ArrowRight"){
+            this.rightpressed = false;
+        }
+        if (event.code == "ArrowLeft"){
+            this.leftpressed = false;
+        }
+        if(event.code == "Space"){
+            this.shootepressed =false;
+        }
+        
+    };
+}
---
+++ SILANDLEVEL/JS5/EnemyController.js
@@ -0,0 +1,217 @@
+import Enemy from "./Enemy.js";
+import MovingDirection from "./MovingDirection.js";
+
+// class of EnemyController
+export default class EnemyController {
+
+    //enemy number of image
+    EnemyMap = [
+
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+        [2, 2, 2, 2, 1, 2, 2, 2, 2, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+        [2, 2, 2, 2, 1, 2, 2, 2, 2, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+        [2, 2, 2, 2, 1, 2, 2, 2, 2, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+        [2, 2, 2, 2, 1, 2, 2, 2, 2, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+        [2, 2, 2, 2, 1, 2, 2, 2, 2, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+        [2, 2, 2, 2, 1, 2, 2, 2, 2, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+        [2, 2, 2, 2, 1, 2, 2, 2, 2, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+        [2, 2, 2, 2, 1, 2, 2, 2, 2, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+        [2, 2, 2, 2, 1, 2, 2, 2, 2, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+        [2, 2, 2, 2, 1, 2, 2, 2, 2, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    ];
+
+    enemyrow = [];
+    //current is first right side
+    currentdirection = MovingDirection.right;
+    xvelocity = 0;
+    yvelocity = 0;
+    defaultXvelocity = 1;
+    defaultYvelocity = 1;
+
+    //control of the time of moving
+    MovingDownTimerD = 50;
+
+    fireBulletTimerD = 10;
+
+    MovingDownTimer = this.MovingDownTimerD;
+
+    fireBulletTimer = this.fireBulletTimerD;
+
+    constructor(canvas, enemyBulletController, playerBulletController) {
+        this.canvas = canvas;
+        this.enemyBulletController = enemyBulletController;
+        this.playerBulletController = playerBulletController;
+        this.enemyDethSound = new Audio ('ss/enemy-death.wav');
+        this.enemyDethSound.volume = 0.5;
+        
+        this.createEnemy();
+    }
+
+    draw(ctx) {
+        this.decrementMoveDirection();
+        //update is move the enemys
+        this.updatevelocityanddirection();
+        this.collisionDetection();
+        this.drawEnemies(ctx);
+        this.resettimer();
+        this.fireBullet();
+    }
+
+    collisionDetection() {
+        this.enemyrow.forEach((enemyrow) => {
+          enemyrow.forEach((enemy, enemyIndex) => {
+            if (this.playerBulletController.collideWith(enemy)) {
+                this.enemyDethSound.currentTime =0;
+                this.enemyDethSound.play();
+              enemyrow.splice(enemyIndex, 1);
+            }
+          });
+        });
+    
+        this.enemyrow = this.enemyrow.filter((enemyrow) => enemyrow.length > 0);
+      }
+
+    fireBullet() {
+        this.fireBulletTimer--;
+        if (this.fireBulletTimer <= 0) {
+            this.fireBulletTimer = this.fireBulletTimerD;
+            const allenemy = this.enemyrow.flat();
+            const enemyIndex = Math.floor(Math.random() * allenemy.length);
+            const enemy = allenemy[enemyIndex];
+            this.enemyBulletController.shoot(enemy.x+enemy.width/2, enemy.y, -4);
+            //console.log(enemyIndex);    
+        }
+    }
+    //resettimer
+    resettimer() {
+        if (this.MovingDownTimer <= 0) {
+            this.MovingDownTimer = this.MovingDownTimerD;
+        }
+    }
+    //decrementmovedirection
+    decrementMoveDirection() {
+        if (this.currentdirection === MovingDirection.downleft ||
+            this.currentdirection === MovingDirection.downright) {
+            this.MovingDownTimer -- ;
+        }
+    }
+    // logic of moving the velocity and direction
+    updatevelocityanddirection() {
+        for (const enemyrow of this.enemyrow) {
+            if (this.currentdirection == MovingDirection.right) {
+                this.xvelocity = this.defaultXvelocity;
+                this.yvelocity = 0;
+                //left side moving 
+                const rightMostEnemy = enemyrow[enemyrow.length - 1];
+                if (rightMostEnemy.x + rightMostEnemy.width >= this.canvas.width) {
+                    this.currentdirection = MovingDirection.downleft;
+                    break;
+                }
+            } else if (this.currentdirection === MovingDirection.downleft) {
+                if (this.movedown(MovingDirection.left)) {
+                    break;
+                }
+            } else if (this.currentdirection === MovingDirection.left) {
+                this.xvelocity = -this.defaultXvelocity;
+                this.yvelocity = 0;
+                const leftMostEnemy = enemyrow[0];
+                if (leftMostEnemy.x <= 0) {
+                    this.currentdirection = MovingDirection.downright;
+                    break;
+                }
+            } else if (this.currentdirection === MovingDirection.downright) {
+                if (this.movedown(MovingDirection.right)) {
+                    break;
+                }
+            }
+
+        }
+    }
+
+    // logic of down the enemy 
+    movedown(newDirection) {
+        this.xvelocity = 0;
+        this.yvelocity = this.defaultYvelocity;
+        if (this.MovingDownTimer <= 0) {
+            this.currentdirection = newDirection;
+            return true;
+        }
+        return false;
+    }
+
+    // draw to and moving 
+    drawEnemies(ctx) {
+        this.enemyrow.flat().forEach((enemy) => {
+            enemy.move(this.xvelocity, this.yvelocity);
+            enemy.draw(ctx);
+        })
+    }
+
+    // create the enemy by bitween spaces
+    createEnemy() {
+        this.EnemyMap.forEach((row, rowIndex) => {
+            this.enemyrow[rowIndex] = [];
+            row.forEach((enemyNumber, enemyIndex) => {
+                if (enemyNumber > 0) {
+                    //crete the enemy for rowindex for number by enemy
+                    this.enemyrow[rowIndex].push(
+                        //enemy space bitween like boss type
+                        new Enemy(enemyIndex * 80, rowIndex * 3, enemyNumber))
+                }
+            })
+        })
+    }
+
+    collideWith(sprite){
+        return this.enemyrow.flat().some((enemy) => enemy.collideWith(sprite));
+    }
+}
